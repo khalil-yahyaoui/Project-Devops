f4c6c4691edd7b2ef20ea96d53dda1cf08494141
khalil-yahyaoui <yahyaoui.khalil@insat.u-carthage.tn>
2022-12-08 22:34:09 +0100

    switch to groovy scripts

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,14 +1,14 @@
 def gitClone(){
-	echo "Getting Project from Git"
-	git "https://github.com/khalil-yahyaoui/Project-Devops"
+	echo "Getting Project from Git";
+	git "https://github.com/khalil-yahyaoui/Project-Devops";
 }
 
 def runMvnClean(){
-	echo "Maven Clean"
-	sh 'mvn clean'
+	echo "Maven Clean";
+	sh 'mvn clean';
 }
 
 def runUnitTests(){
-	echo "Maven Test JUnit"
-	sh 'mvn test'
+	echo "Maven Test JUnit";
+	sh 'mvn test';
 }
